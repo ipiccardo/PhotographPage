cacadad06c8136bda7d8eb798657295238d7bde8
ipiccard <ivan.piccardo@tsoftlatam.com>
2022-11-09 19:56:22 -0300

    Agrego texto al homescreen

--- src/Components/HomeScreen/HomeScreen.jsx
+++ src/Components/HomeScreen/HomeScreen.jsx
@@ -5,6 +5,15 @@ export const HomeScreen = () => {
   return (
     <>
       <div className="hero"></div>
+      <div className="title-box">
+        <h1>Alexis Fernández</h1>
+        <p>
+          «No haces fotografía sólo con la cámara. La haces con
+          todas las imágenes que has visto, con todos los libros que has leído,
+          con toda la música que has escuchado, y con toda la gente a la que has
+          amado» Ansel Adams
+        </p>
+      </div>
     </>
   );
 };
