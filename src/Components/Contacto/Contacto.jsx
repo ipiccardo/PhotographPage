0985541ce1c1056b0e388d72e3d6c5ab2e858b20
ipiccard <ivan.piccardo@tsoftlatam.com>
2022-11-11 19:38:18 -0300

    Agrego contaco

--- src/Components/Contacto/Contacto.jsx
+++ src/Components/Contacto/Contacto.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faE, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import "./contacto.css";
 
 export const Contacto = () => {
   return (
-    <div>Contacto</div>
-  )
-}
+    <>
+      <h1>CONTACTO</h1>
+      <div className="social-media-container" style={{ fontSize: "4em" }}>
+        <a
+            rel="noreferrer"
+            href="https://instagram.com/chinofernandezph?igshid=YmMyMTA2M2Y="
+            target="_blank"
+          >
+        <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        <a
+          rel="noreferrer"
+          href="https://wa.me/5491144181310"
+          target="_blank"
+          title="5491144181310"
+        >
+          <FontAwesomeIcon icon={faWhatsapp} />
+        </a>
+        <a
+          rel="noreferrer"
+          href="https://mail@gmail.com"
+          target="_blank"
+          title="mail@gmail.com"
+        >
+          <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+        </a>
+      </div>
+    </>
+  );
+};
 
-export default Contacto
+export default Contacto;
