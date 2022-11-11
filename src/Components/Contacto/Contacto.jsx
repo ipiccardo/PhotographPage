6bbecca9e18cb914a87ee1caab6eaefdc17636b2
ipiccard <ivan.piccardo@tsoftlatam.com>
2022-11-11 19:53:46 -0300

    quito el fa

--- src/Components/Contacto/Contacto.jsx
+++ src/Components/Contacto/Contacto.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
-import { faE, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import "./contacto.css";
 
 export const Contacto = () => {
