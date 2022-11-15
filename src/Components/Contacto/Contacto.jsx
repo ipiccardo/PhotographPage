a0e6c1230cbe60593cf4bcc6639cd9aded661174
ipiccard <ivan.piccardo@tsoftlatam.com>
2022-11-15 11:30:38 -0300

    Alineo los links al centro

--- src/Components/Contacto/Contacto.jsx
+++ src/Components/Contacto/Contacto.jsx
@@ -8,45 +8,47 @@ import Footer from "../Footer/Footer";
 export const Contacto = () => {
   return (
     <>
-    <div>
-      <h1>CONTACTO</h1>
-      <div className="social-media-container" style={{ fontSize: "4em"}}>
-        <div className="social-media-div">
-        <span>chinofernandezph</span>
-        <a
-          rel="noreferrer"
-          href="https://instagram.com/chinofernandezph?igshid=YmMyMTA2M2Y="
-          target="_blank"
-          >
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
+      <div>
+        <h1>CONTACTO</h1>
+        <div className="social-media-container" style={{ fontSize: "4em" }}>
+          <div className="social-media-div">
+            <span>chinofernandezph</span>
+            <a
+              className="social-media-links"
+              rel="noreferrer"
+              href="https://instagram.com/chinofernandezph?igshid=YmMyMTA2M2Y="
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
           </div>
           <div className="social-media-div">
-
-        <span>5491144181310</span>
-        <a
-          rel="noreferrer"
-          href="https://wa.me/5491144181310"
-          target="_blank"
-          title="5491144181310"
-          >
-          <FontAwesomeIcon icon={faWhatsapp} />
-        </a>
+            <span>5491144181310</span>
+            <a
+              className="social-media-links"
+              rel="noreferrer"
+              href="https://wa.me/5491144181310"
+              target="_blank"
+              title="5491144181310"
+            >
+              <FontAwesomeIcon icon={faWhatsapp} />
+            </a>
           </div>
           <div className="social-media-div">
-        <span>alexisdariofernandez@gmail.com</span>
-        <a
-          rel="noreferrer"
-          href="https://alexisdariofernandez@gmail.com"
-          target="_blank"
-          title="alexisdariofernandez@gmail.com"
-          >
-          <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
-        </a>
+            <span>alexisdariofernandez@gmail.com</span>
+            <a
+              className="social-media-links"
+              rel="noreferrer"
+              href="https://alexisdariofernandez@gmail.com"
+              target="_blank"
+              title="alexisdariofernandez@gmail.com"
+            >
+              <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+            </a>
+          </div>
         </div>
       </div>
-          </div>
-        <Footer />
+      <Footer />
     </>
   );
 };
